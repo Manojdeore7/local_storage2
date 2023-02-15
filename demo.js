b663228d30d5aa7d2ded58131a2ce80469fb42e1
Manoj Deore <112100835+Manojdeore7@users.noreply.github.com>
2023-02-15 12:10:25 +0530

    local storage 2 is raedy

---
+++ demo.js
@@ -0,0 +1,41 @@
+let nme=document.getElementById("name");
+let email=document.getElementById("email");
+let sub=document.getElementById("submit");
+var arr=JSON.parse(localStorage.getItem("data"))||[];
+sub.addEventListener("click",(ex)=>{
+    ex.preventDefault();
+    let obj={
+        name:nme.value,
+        email:email.value
+    };
+    
+    arr.push(obj);
+    localStorage.setItem("data",JSON.stringify(arr));
+    let tbody=document.getElementById("tbody");
+    tbody.innerHTML="";
+    arr.forEach((e)=>{
+    let row=document.createElement("tr");
+    let col1=document.createElement("td");
+    col1.innerText=e.name;
+    let col2=document.createElement("td");
+    col2.innerText=e.email;
+    let col3=document.createElement("td");
+    col3.innerText="delete";
+    //col3.className="btn btn-danger";
+    
+    col3.addEventListener("click",(el)=>{
+        el.target.parentNode.parentNode.removeChild(el.target.parentNode);
+       let ar=JSON.parse(localStorage.getItem("data"))||[];
+       arr=ar;
+        let a=ar.filter((ele)=>{
+            return e.name!=ele.name;
+        })
+        localStorage.setItem("data",JSON.stringify(a));
+    })
+    row.append(col1,col2,col3);
+    tbody.append(row);
+})
+})
+
+//deplay data on ui
+
